5cf2bd68e014a6385989246a2ea7875c3674bf9d
Daan Bakker <bluedaan@gmail.com>
2013-04-07 20:50:20 +0200

    Download plugins in zip

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # Usage: install.sh
 
-BUNDLE_URL=$1
-BUNDLE_FILE=~/.vim/local/bundle.tar.xz
+BUNDLE_FILE=~/.cache/vim/bundle.zip
 
 if [ ! -d ~/.vim ]; then
   git clone --recursive https://github.com/dbakker/vimfiles.git ~/.vim || exit 1
@@ -17,11 +16,8 @@ fi
 
 [ -f ~/.vimrc ] || ln -s ~/.vim/.vimrc ~/.vimrc
 
-if [ -d ~/.vim/bundle/.git ]; then
-  pushd ~/.vim/bundle>/dev/null
-  git pull origin master -f
-  popd>/dev/null
-else
-  [ -d ~/.vim/bundle ] && rm -rf ~/.vim/bundle
-  git clone https://github.com/dbakker/vimfiles-plugins ~/.vim/bundle
-fi
+[ -d ~/.vim/bundle ] && rm -rf ~/.vim/bundle
+[ -d ~/.cache/vim ] || mkdir -p ~/.cache/vim
+curl -s -o $BUNDLE_FILE -L https://github.com/dbakker/vimfiles-plugins/archive/master.zip
+unzip -d ~/.vim -qq $BUNDLE_FILE
+mv ~/.vim/vimfiles-plugins-master ~/.vim/bundle
