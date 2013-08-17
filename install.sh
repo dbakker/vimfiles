929d695bb31b3e91b644037a861f74bafdc9ec74
Daan Bakker <bluedaan@gmail.com>
2013-08-17 12:27:35 +0200

    Do not allow installing this under the root user

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Usage: install.sh
 
+[ $USER == "root" ] && echo You should not install this for the root account. && exit 1
+
 if [ ! -d ~/.vim ]; then
   git clone --recursive https://github.com/dbakker/vimfiles.git ~/.vim || exit 1
   git clone --recursive https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim || exit 1
